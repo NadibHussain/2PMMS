b08b995369046b1b94e386657e25ae4c1133b570
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2022-02-16 13:37:02 -0500

    added sort_chunk method for sorting each chunk of array

--- src/main/java/core/index.java
+++ src/main/java/core/index.java
@@ -117,6 +117,83 @@ public class index {
             e.printStackTrace();
         }
     }
+
+    int[] sort_chunk(int [][]chunk){
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for(int i=0; i< chunk.length; i++){
+            for(int val: chunk[i]){
+                arrayList.add(val);
+            }
+        }
+
+        int array[] = new int[arrayList.size()];
+        int c=0;
+        for(int el: arrayList){
+            array[c]=el;
+            c++;
+        }
+        int sorted[] = mergeSort(array, 0, array.length-1);
+
+        System.out.println("Printing sorted[] from sort_chunk method: ");
+        for(int val: sorted){
+            System.out.print(val+" ");
+        }
+
+        return sorted;
+    }
+    void merge(int arr[], int p, int q, int r) {
+
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[p + i];
+        for (int j = 0; j < n2; j++)
+            M[j] = arr[q + 1 + j];
+
+        int i = 0;
+        int j = 0;
+        int k = p;
+
+        //merging
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+        //merging of rest of the elements that was not stored already
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = M[j];
+            j++;
+            k++;
+        }
+    }
+
+    int [] mergeSort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean exit = false;
